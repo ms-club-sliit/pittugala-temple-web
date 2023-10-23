4bf4bc6795b28d118efc30932b125aa2e6a9424b
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-10-23 19:45:59 +0530

    Mui Image gallery added

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -5,6 +5,8 @@ import GalleryNavigation from '@/components/gallery/gallerynavigation';
 import Box from '@mui/material/Box';
 import React from 'react';
 import "../../../src/CSS/gallery.css";
+import { ImageList, ImageListItem,Container } from '@mui/material';
+
 
 
 //Dummy Date for Gallery Page
@@ -43,13 +45,13 @@ const galleryData = {
             "category": "poya_day",
             "image": "/static/Gallery.png",
             "width": "440",
-            "height": "300",
+            "height": "320",
         },
         {
             "id": 6,
             "category": "katina_pooja",
             "image": "/static/Gallery.png",
-            "width": "500",
+            "width": "600",
             "height": "400",
         },
         {
@@ -79,29 +81,45 @@ const Gallery = () => {
 
     return (
         <div
-        style={{
-       
-            backgroundColor: "white",
+            style={{
 
-        }}
+                backgroundColor: "white",
+
+            }}
         >
             <Navbar />
             <GalleryNavigation />
             <hr></hr>
-            <div className="gallery-container"  style={{
+           
+<Container maxWidth="100vw" sx={{width: "90vw", marginRight: "2%",marginTop:"2%"}} >
+<ImageList sx={{ width: 'auto', height: 'auto' }} variant="woven" cols={3} gap={8}>
+  {galleryData?.gallery.map((item) => (
+    <ImageListItem key={item.img}>
+      <img
+        srcSet={`${item.image}?w=161&fit=crop&auto=format&dpr=2 2x`}
+        src={`${item.image}?w=161&fit=crop&auto=format`}
+        alt={item.title}
+        loading="lazy"
+      />
+    </ImageListItem>
+  ))}
+</ImageList>
+</Container>
+
+            {/* <div className="gallery-container"  style={{
             marginLeft: "8%",
             marginTop: "2%",
           }}>
         {galleryData.gallery.map((item, index) => (
-          <div key={item.id} className="gallery-item">
+          <div key={item.id} className="gallery-item" style={{ flex: `0 0 calc(33.33% - 10px)`, margin: "5px" }}>
             <img
               src={item.image}
               alt={`Image ${item.id}`}
-              style={{ width: `${item.width}px`, height: `${item.height}px`, marginTop: "2%" }}
+              style={{ width: "auto", height: `${item.height}px`, marginTop: "2%" }}
             />
           </div>
         ))}
-      </div>
+      </div> */}
             <hr></hr>
             <Footer />
         </div>
