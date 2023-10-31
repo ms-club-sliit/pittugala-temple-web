f8772753082cb139f0fed615d606f5ace311666d
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-10-31 08:38:13 +0530

    Filtering done

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,112 +1,142 @@
+"use client";
 import Footer from '@/components/footer'
 import Image from 'next/image'
 import Navbar from "@/components/Navbar";
 import GalleryNavigation from '@/components/gallery/gallerynavigation';
 import Box from '@mui/material/Box';
-import React from 'react';
+import React, { useState } from 'react';
 import "../../../src/CSS/gallery.css";
-import { ImageList, ImageListItem,Container } from '@mui/material';
+import { ImageList, ImageListItem, Container } from '@mui/material';
 
 
 
 //Dummy Date for Gallery Page
 const galleryData = {
-    "gallery": [
-        {
-            "id": 1,
-            "category": "poya_day",
-            "image": "/static/Gallery.png",
-            "width": "617",
-            "height": "300",
-        },
-        {
-            "id": 2,
-            "category": "katina_pooja",
-            "image": "/static/Gallery.png",
-            "width": "617",
-            "height": "400",
-        },
-        {
-            "id": 3,
-            "category": "bodhi_pooja",
-            "image": "/static/Gallery.png",
-            "width": "480",
-            "height": "600",
-        },
-        {
-            "id": 4,
-            "category": "charity",
-            "image": "/static/Gallery.png",
-            "width": "440",
-            "height": "400",
-        },
-        {
-            "id": 5,
-            "category": "poya_day",
-            "image": "/static/Gallery.png",
-            "width": "440",
-            "height": "320",
-        },
-        {
-            "id": 6,
-            "category": "katina_pooja",
-            "image": "/static/Gallery.png",
-            "width": "600",
-            "height": "400",
-        },
-        {
-            "id": 7,
-            "category": "bodhi_pooja",
-            "image": "/static/Gallery.png",
-            "width": "450",
-            "height": "300",
-        },
-        {
-            "id": 8,
-            "category": "charity",
-            "image": "/static/Gallery.png",
-            "width": "450",
-            "height": "400",
-        },
-        {
-            "id": 9,
-            "category": "poya_day",
-            "image": "/static/Gallery.png",
-            "width": "450",
-            "height": "600",
-        }
-    ]
+  "gallery": [
+    {
+      "id": 1,
+      "category": "poya_day",
+      "image": "/static/Gallery.png",
+      "width": "617",
+      "height": "300",
+    },
+    {
+      "id": 2,
+      "category": "katina_pooja",
+      "image": "/static/Temple1.png",
+      "width": "617",
+      "height": "400",
+    },
+    {
+      "id": 3,
+      "category": "bodhi_pooja",
+      "image": "/static/Gallery.png",
+      "width": "480",
+      "height": "600",
+    },
+    {
+      "id": 4,
+      "category": "charity",
+      "image": "/static/Temple1.png",
+      "width": "440",
+      "height": "400",
+    },
+    {
+      "id": 5,
+      "category": "poya_day",
+      "image": "/static/Gallery.png",
+      "width": "440",
+      "height": "320",
+    },
+    {
+      "id": 6,
+      "category": "katina_pooja",
+      "image": "/static/Temple1.png",
+      "width": "600",
+      "height": "400",
+    },
+    {
+      "id": 7,
+      "category": "bodhi_pooja",
+      "image": "/static/Gallery.png",
+      "width": "450",
+      "height": "300",
+    },
+    {
+      "id": 8,
+      "category": "charity",
+      "image": "/static/Temple1.png",
+      "width": "450",
+      "height": "400",
+    },
+    {
+      "id": 9,
+      "category": "poya_day",
+      "image": "/static/Gallery.png",
+      "width": "450",
+      "height": "600",
+    }
+  ]
 };
+
+
 const Gallery = () => {
 
-    return (
-        <div
-            style={{
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
-                backgroundColor: "white",
+  const handleCategoryChange = (category) => {
+    console.log("gallerDate",category);
+    setSelectedCategory(category);
+  };
 
-            }}
-        >
-            <Navbar />
-            <GalleryNavigation />
-            <hr></hr>
-           
-<Container maxWidth="100vw" sx={{width: "90vw", marginRight: "2%",marginTop:"2%"}} >
-<ImageList sx={{ width: 'auto', height: 'auto' }} variant="woven" cols={3} gap={8}>
-  {galleryData?.gallery.map((item) => (
-    <ImageListItem key={item.img}>
-      <img
-        srcSet={`${item.image}?w=161&fit=crop&auto=format&dpr=2 2x`}
-        src={`${item.image}?w=161&fit=crop&auto=format`}
-        alt={item.title}
-        loading="lazy"
-      />
-    </ImageListItem>
-  ))}
-</ImageList>
-</Container>
+  const filteredGalleryData = selectedCategory === 'All' ? galleryData.gallery : galleryData.gallery.filter(item => item.category === selectedCategory);
+  console.log("Filtered Gallery Data:", filteredGalleryData); 
 
-            {/* <div className="gallery-container"  style={{
+  console.log("Props being passed to GalleryNavigation - handleCategoryChange:", handleCategoryChange);
+  console.log("Props being passed to GalleryNavigation - selectedCategory:", selectedCategory);
+
+
+  return (
+    <div
+      style={{
+
+        backgroundColor: "white",
+
+      }}
+    >
+      <Navbar />
+      <GalleryNavigation handleCategoryChange={handleCategoryChange} selectedCategory={selectedCategory} />
+      <hr></hr>
+
+      <Container maxWidth="100vw" sx={{ width: "90vw", marginRight: "2%", marginTop: "2%", marginBottom: "2%" }} >
+
+        <ImageList sx={{ width: 'auto', height: 'auto' }} variant="woven" cols={3} gap={8}>
+          {filteredGalleryData.map((item) => ( // Use filteredGalleryData instead of galleryData.gallery
+            <ImageListItem key={item.id}>
+              <img
+                srcSet={`${item.image}?w=${item.width}&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.image}?w=${item.width}&fit=crop&auto=format`}
+                alt={`Image ${item.id}`}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Container>
+      {/* <ImageList sx={{ width: 'auto', height: 'auto' }} variant="woven" cols={3} gap={8}>
+          {galleryData?.gallery.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                srcSet={`${item.image}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.image}?w=161&fit=crop&auto=format`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList> */}
+
+      {/* <div className="gallery-container"  style={{
             marginLeft: "8%",
             marginTop: "2%",
           }}>
@@ -120,10 +150,10 @@ const Gallery = () => {
           </div>
         ))}
       </div> */}
-            <hr></hr>
-            <Footer />
-        </div>
-    );
+      <hr></hr>
+      <Footer />
+    </div>
+  );
 };
 
 export default Gallery;
--- src/components/gallery/gallerynavigation.js
+++ src/components/gallery/gallerynavigation.js
@@ -1,75 +1,92 @@
-// components/gallery_sub_navigation/galleynavigation.js
 "use client";
-import { useState } from "react";
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { styled } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
+import styled from 'styled-components';
 
 const CustomTextField = styled(TextField)({
-    '& .MuiOutlinedInput-root': {
-        borderRadius: '20px',
-    },
-    '& .MuiOutlinedInput-input': {
-        padding: '10px 14px',
-        width: '300px',
-    },
-    '& .MuiInputLabel-outlined': {
-        transform: 'translate(14px, 14px) scale(1)',
-    },
-    '& .custom-end-icon': {
-        width: '24px', 
-        height: '24px',
-        cursor: 'pointer', 
-      },
+  '& .MuiOutlinedInput-root': {
+    borderRadius: '20px',
+  },
+  '& .MuiOutlinedInput-input': {
+    padding: '10px 14px',
+    width: '300px',
+  },
+  '& .MuiInputLabel-outlined': {
+    transform: 'translate(14px, 14px) scale(1)',
+  },
+  '& .custom-end-icon': {
+    width: '24px',
+    height: '24px',
+    cursor: 'pointer',
+  },
 });
 
-const GalleryNavigation = () => {
-    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+const GalleryNavigation = styled.div`
+  .selected {
+    background-color: maroon;
+    color: white;
+    padding: 8px 12px;
+    border-radius: 4px;
+  }
+  .unselected {
+    padding: 8px 12px;
+  }
 
-    const toggleMobileMenu = () => {
-        setMobileMenuOpen(!isMobileMenuOpen);
-    };
+  .selected a {
+    color: white;
+    text-decoration: none;
+  }
+`;
 
-    return (
-        <nav className="bg-white p-4 text-black font-semibold">
-            <div className="container mx-auto flex justify-between items-center pl-20">
-                <div className="hidden md:block">
-                    <ul className="flex space-x-6 pr-20">
-                        <li>
-                            <a href="#">All</a>
-                        </li>
-                        <li className="pl-5">
-                            <a href="#">Poya Day</a>
-                        </li>
-                        <li className="pl-5">
-                            <a href="#">Katina Pooja</a>
-                        </li>
-                        <li className="pl-5">
-                            <a href="#">Bodhi Pooja</a>
-                        </li>
-                        <li className="pl-5">
-                            <a href="#">Charity</a>
-                        </li>
-                    </ul>
-                </div>
-                {/* <div className="md:hidden flex items-center">
-          <button
-            className="animate-bounce text-black"
-            onClick={toggleMobileMenu}
-          >
-            &#9776;
-          </button>
-        </div> */}
-                <CustomTextField
-                    id="outlined-basic"
-                    variant="outlined"
-                    InputProps={{
-                        endAdornment:  <SearchIcon style={{ color: 'grey' }} />,
-                      }}
-                />
-            </div>
-        </nav>
-    );
+const NavigationItem = styled.li`
+  cursor: pointer;
+`;
+
+const GalleryNavItem = ({ selectedItem, handleItemClick, itemName, children }) => (
+  <NavigationItem className={selectedItem === itemName ? 'selected' : 'unselected'} onClick={() => handleItemClick(itemName)}>
+  <a href="#" onClick={(e) => { e.preventDefault(); handleItemClick(itemName); }}>{children}</a>
+  </NavigationItem>
+);
+
+const GalleryNavigationComponent = ({ handleCategoryChange, selectedCategory }) => {
+
+  const handleItemClick = (itemName) => {
+    handleCategoryChange(itemName);
+  };
+
+  return (
+    <GalleryNavigation className="bg-white p-4 text-black font-semibold">
+      <div className="container mx-auto flex justify-between items-center pl-20">
+        <div className="hidden md:block">
+          <ul className="flex space-x-6 pr-20">
+            <GalleryNavItem selectedItem={selectedCategory} handleItemClick={handleItemClick} itemName="All">
+              All
+            </GalleryNavItem>
+            <GalleryNavItem selectedItem={selectedCategory} handleItemClick={handleItemClick} itemName="poya_day">
+              Poya Day
+            </GalleryNavItem>
+            <GalleryNavItem selectedItem={selectedCategory} handleItemClick={handleItemClick} itemName="katina_pooja">
+              Katina Pooja
+            </GalleryNavItem>
+            <GalleryNavItem selectedItem={selectedCategory} handleItemClick={handleItemClick} itemName="bodhi_pooja">
+              Bodhi Pooja
+            </GalleryNavItem>
+            <GalleryNavItem selectedItem={selectedCategory} handleItemClick={handleItemClick} itemName="charity">
+              Charity
+            </GalleryNavItem>
+          </ul>
+        </div>
+        <CustomTextField
+          id="outlined-basic"
+          variant="outlined"
+          InputProps={{
+            endAdornment: <SearchIcon style={{ color: 'grey' }} />,
+          }}
+        />
+      </div>
+    </GalleryNavigation>
+  );
 };
 
-export default GalleryNavigation;
+export default GalleryNavigationComponent;
