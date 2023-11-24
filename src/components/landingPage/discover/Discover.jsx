f2cf4f0dce323ec2eb592006a36fced9dde033d5
ChathukaViduranga <chathuka.viduranga.bc@gmail.com>
2023-11-24 21:14:10 +0530

    error resloved

--- src/components/landingPage/discover/Discover.jsx
+++ src/components/landingPage/discover/Discover.jsx
@@ -1,8 +1,6 @@
 'use client'
-import React from 'react'
 import { useRouter } from 'next/navigation'
-
-export default function index() {
+export default function Discover() {
   const router = useRouter()
   return (
     <div className="bg-white">
