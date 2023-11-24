d417cd2527c57f462724cd9269fdf88b32a2d273
ChathukaViduranga <chathuka.viduranga.bc@gmail.com>
2023-11-24 21:19:50 +0530

    added color to event

--- src/components/landingPage/events/index.jsx
+++ src/components/landingPage/events/index.jsx
@@ -28,7 +28,7 @@ export default function index() {
   ]
 
   return (
-    <div className="p-8 hidden sm:flex flex-col place-items-center w-full ">
+    <div className="p-8 hidden sm:flex flex-col place-items-center w-full bg-white ">
       <h1 className="text-4xl font-bold mb-8">Events</h1>
       <div className="  grid grid-cols-4   w-11/12 drop-shadow">
         {events.map((event) => (
