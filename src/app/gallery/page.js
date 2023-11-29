856f5542bf37971edcfd2e19766a27928046eb2a
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-11-29 12:25:09 +0530

    Issue resolved and Update images
    1. Resolved the images being stretched when filtered.
    2. Updated new images

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -2,85 +2,11 @@
 import Image from 'next/image'
 import GalleryNavigation from '@/components/gallery/gallerynavigation'
 import Box from '@mui/material/Box'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../../src/CSS/gallery.css'
 import { ImageList, ImageListItem, Container } from '@mui/material'
+import galleryData from '@/data/gallery.json'
 
-//Dummy Date for Gallery Page
-const galleryData = {
-  gallery: [
-    {
-      id: 1,
-      category: 'poya_day',
-      image: '/static/Gallery.png',
-      width: '617',
-      height: '400',
-    },
-    {
-      id: 2,
-      category: 'katina_pooja',
-      image: '/static/Temple1.png',
-      width: '617',
-      height: '400',
-    },
-    {
-      id: 3,
-      category: 'bodhi_pooja',
-      image: '/static/Gallery.png',
-      width: '617',
-      height: '400',
-    },
-    {
-      id: 4,
-      category: 'charity',
-      image: '/static/Temple1.png',
-      width: '617',
-      height: '400',
-    },
-    {
-      id: 5,
-      category: 'poya_day',
-      image: '/static/Gallery.png',
-      width: '617',
-      height: '400',
-    },
-    {
-      id: 6,
-      category: 'katina_pooja',
-      image: '/static/Temple1.png',
-      width: '600',
-      height: '400',
-    },
-    {
-      id: 7,
-      category: 'bodhi_pooja',
-      image: '/static/Gallery.png',
-      width: '617',
-      height: '400',
-    },
-    {
-      id: 8,
-      category: 'charity',
-      image: '/static/Temple1.png',
-      width: '617',
-      height: '400',
-    },
-    {
-      id: 9,
-      category: 'poya_day',
-      image: '/static/Gallery.png',
-      width: '617',
-      height: '400',
-    },
-    {
-      id: 10,
-      category: 'poya_day',
-      image: '/static/Temple1.png',
-      width: '617',
-      height: '400',
-    },
-  ],
-}
 
 const Gallery = () => {
   const [selectedCategory, setSelectedCategory] = useState('All')
@@ -91,8 +17,10 @@ const Gallery = () => {
 
   const filteredGalleryData =
     selectedCategory === 'All'
-      ? galleryData.gallery
-      : galleryData.gallery.filter((item) => item.category === selectedCategory)
+      ? galleryData.galleryData
+      : galleryData.galleryData.filter((item) => item.category === selectedCategory)
+
+  const numberOfImages = filteredGalleryData.length;
 
   return (
     <div className="bg-white">
@@ -101,22 +29,45 @@ const Gallery = () => {
         selectedCategory={selectedCategory}
       />
       <hr></hr>
-      <div className="grid grid-cols-3 gap-4 w-10/12 ml-[180px] mb-10 mt-10 ">
-        {filteredGalleryData.map((item, index) => (
-          <div
-            key={item.id}
-            className={`${
-              index === 1 || index === 5 ? 'col-span-2' : 'col-span-1'
-            }`}
-          >
-            <img
-              src={item.image}
-              alt={item.category}
-              className="w-full rounded-lg w-full h-full"
-            />
-          </div>
-        ))}
-      </div>
+      {numberOfImages !== 1 && numberOfImages < 5 ? (
+        <div className="grid grid-cols-3 gap-4 w-10/12 ml-[180px] mb-10 mt-10 ">
+
+          {filteredGalleryData.map((item, index) => (
+            <div
+              key={item.id}
+              className={`${index === 0 || index === 2 ? 'col-span-2' : 'col-span-1'
+                }`}
+            >
+              <img
+                src={`/static/gallery-images/${item.image}`}
+                alt={item.category}
+                className="w-full rounded-lg w-full h-full"
+              />
+            </div>
+
+          ))}
+
+        </div>
+      ) : (
+        <div className="grid grid-cols-3 gap-4 w-10/12 ml-[180px] mb-10 mt-10 ">
+
+          {filteredGalleryData.map((item, index) => (
+            <div
+              key={item.id}
+              className={`${index === 1 || index === 5 ? 'col-span-2' : 'col-span-1'
+                }`}
+            >
+              <img
+                src={`/static/gallery-images/${item.image}`}
+                alt={item.category}
+                className="w-full rounded-lg w-full h-full"
+              />
+            </div>
+
+          ))}
+
+        </div>
+      )}
       <hr></hr>
     </div>
   )
