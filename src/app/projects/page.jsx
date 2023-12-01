685c87f3d7eb968576ebc246d3122b4b1d0eb20b
Randula Mawaththta <randula98@gmail.com>
2023-12-01 23:50:48 +0530

    Project & Donation Sections Implemented

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -5,7 +5,7 @@ const DhammaSchoolPage = () => {
       <div className="text-black container mx-auto px-20">
         <div className="block mx-auto my-12 font-semibold w-full lg:w-2/4">
           <p className="text-center text-4xl lg:text-5xl mb-4 leading-normal">
-            Construction of Dhatu Mandir and Sanghavasa
+            නව ධාතු මන්දිරය සහ සංඝාවාසයේ ඉදිකිරිම්
           </p>
         </div>
         <div className="block my-12">
@@ -17,11 +17,16 @@ const DhammaSchoolPage = () => {
         </div>
         <div className="block my-12">
           <p className="text-base">
-            Construction of Dhatu Mandir and Sanghavasa Since the construction
-            work of the new Dhatu Mandir and Sanghavasa has already started, we
-            cordially invite you to contribute to this noble task of completing
-            its work. <br /> Donations of money Bank of Ceylon Malabe Branch -
-            85769121 <br /> Abbot - Sri Sudarshanarama Puran Vihara - Malabe |
+            මෙම ඉපැරිණි සිද්ධස්ථානයේ පවතින සමහර ගොඩනැගිලි මේ වන විට අබලන් තත්වයට පත්ව ඇත.
+            විශේෂයෙන් භික්ෂුන්ව හන්සේලා වැඩ වෙසෙන සංඝාවාසය භාවිතයට නුසුදුසු ආකාරයට අබලන් වී ඇත.
+            එබැවින් මෙම සංඝාවාසය කඩා ඉවත් කොට නව සංඝාවාසයක් ඉදිකිරීම කඩිනම් අවශ්‍යතාවක් ලෙස විහාරාරස්ථානය හඳුනා ගෙන ඇත.
+            නව ධාතු මන්දිරය සහ සංඝාවාසයේ ඉදිකිරිම් කටයුතු දැනටමත් ආරම්භ කර ඇති බැවින්එහි වැඩ නිමකරවාගැනීමේ මෙම උතුම් කර්තව්‍යට දායකත්වය
+            ලබා දෙන ලෙසට ශාසනාලයෙන් ආරාධනා කරමු.
+
+            <br /> <br />
+            විහාරාධිපති <br />
+            ශ්‍රී සුදර්ශනාරාම පුරාණ විහාරය - මාලබේ<br />
+            ලංකා බැංකුව මාලබේ ශාඛව - 8576912<br />
             0715608209
           </p>
         </div>
