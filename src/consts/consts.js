b891d7c29c1df6ae1a59f7290953278e821b5b2b
Randula Mawaththta <randula98@gmail.com>
2023-12-01 21:17:11 +0530

    About Content Added

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -1,3 +1,8 @@
-export const ABOUT_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula libero nec augue molestie, non interdum tellus porttitor. Sed non ultrices dui. Cras auctor porttitor turpis in feugiat. Integer feugiat, ipsum quis commodo pharetra, nisl felis bibendum libero, eu congue libero nisl et nulla. Morbi a interdum diam, ut elementum sem. Phasellus vulputate egestas lacus a ultricies. Proin vestibulum tempus quam ac condimentum. Vivamus at nibh quis tortor posuere vehicula ut sit amet augue. Quisque risus enim, tristique at dolor nec, hendrerit hendrerit massa. In libero ex, euismod at commodo in, accumsan id tortor. Ut vulputate maximus risus, quis sagittis nisl posuere a. Maecenas ac eros a orci dapibus."
-export const ABOUT_POTRAIT_TEXT = "Most Venerable Tibbatuwawe Sri Siddhartha Sumangalabidana MahanayakeThero Malwatta Chapter"
+export const ABOUT_TEXT = "මාලබේ පිට්ටුගල ශ්‍රී සුදර්ශනාරාමය අවුරුදු 300 කට වඩා පැරණි පුරාණ මහා විහාරයකි. මෙම විහාරය කලාපයේ පැරණිතම ආගමික විහාරස්ථානයකි. එහි වර්තමාන විහාරාධිපති මුල්ලේරියාව පාර්ශවයේ ප්‍රධාන සංඝනායක පනලීයේ මංගල ඉස්ථවීර වහන්සේ මෙම විහාරස්ථානයේ පස්වැනි විහාරාධිපතින් වහන්සේය"
+
+export const ABOUT_POTRAIT_TEXT = "වත්මන්  විහාරධිපති   : මාලඹේ ශ්‍රී සුදර්ශනාරාම පුරාණ විහාරයේ මහා විහාරාධිපති මුල්ලේරියාව පාර්ශවය ප්‍රධාන සංඝනායක පනලීයේ මංගල හිමියන්"
 export const PROJECT_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula libero nec augue molestie, non interdum tellus porttitor. Sed non ultrices dui. Cras auctor porttitor turpis in feugiat. Integer feugiat, ipsum quis commodo pharetra, nisl felis bibendum libero, eu congue libero nisl et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula"
+
+
+export const DONATION_HEADER = "Donation"
+
